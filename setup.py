2ae8b0adb79622f459873579bb78da7bb5ad2404
Eryk Humberto Oliveira Alves <ealves@stone.com.br>
2017-07-08 17:31:56 -0300

    Remove README from setup.py load for long_description

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
-import os
 from setuptools import setup
 
-
-# Utility function to read the README file.  
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(name='dict-table',
-      long_description=read('README.md'),
       version='0.0.1',
       author='Eryk Humberto Oliveira Alves',
       author_email='erykwho@gmail.com',
