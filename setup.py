66a6cb38f29d09dabb5fd59b3ea1554699bb4820
Eryk Humberto Oliveira Alves <ealves@stone.com.br>
2017-07-08 17:48:20 -0300

    Add packages to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='dict-table',
-      version='0.0.1',
+      version='0.0.4',
       author='Eryk Humberto Oliveira Alves',
       author_email='erykwho@gmail.com',
       url='https://github.com/otrabalhador/dict-table/',
-      packages=['dict_table', 'dict_table.utils'],
+      packages=['dict_table', 'dict_table.utils', 'dict_table.storage', 'dict_table.tests'],
       keywords='dict table query',
       classifiers=[
           "Topic :: Utilities",
