84b5f23382a1956bc91a18e80d070f7f42621845
Eryk Humberto Oliveira Alves <ealves@stone.com.br>
2017-07-08 16:31:44 -0300

    Organize directories for PyPI setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+
+setup(name='dict-table',
+      version='0.0.1',
+      author='Eryk Humberto Oliveira Alves',
+      author_email='erykwho@gmail.com',
+      url='https://github.com/otrabalhador/dict-table/',
+      packages=['dict_table', 'dict_table.utils'],
+      install_requires='',
+      extras_require='',
+      keywords='dict table query')
